99da0aba8cef20ced135f9b5b5b690cebe97ee10
jinee5232@gmail.com <jingyee0827@gmail.com>
2024-03-01 20:10:35 +0800

    change src

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Carousel from "../components/carousel";
+import Carousel from "../components/carousel.jsx";
 import "../styles/homepage.css";
 
 const Homepage = () => {
