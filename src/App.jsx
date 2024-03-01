f4a9cd876993e89d562fd1bcf4b1ac139709b60f
jinee5232@gmail.com <jingyee0827@gmail.com>
2024-03-01 22:08:10 +0800

    clear app.css

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,10 @@ import Homepage from "./pages/Homepage.jsx";
 import MapPage from "./pages/MapPage.jsx";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-// import "./App.css";
+import "./App.css";
 import "./styles/style.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <HashRouter>
