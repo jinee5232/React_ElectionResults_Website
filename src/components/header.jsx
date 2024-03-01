01dfa817eed3866971f2c0bfbb9b3f8db70102b0
jinee5232@gmail.com <jingyee0827@gmail.com>
2024-03-01 21:15:32 +0800

    %PUBLIC_URL%

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/header.css";
-import Bigicon from "/icon/Svg/font_big.svg";
-import Smallicon from "/icon/Svg/font_small.svg?react";
-import Shareicon from "/icon/Svg/share.svg?react";
+import Bigicon from "../assets/Svg/font_big.svg";
+import Smallicon from "../assets/Svg/font_small.svg?react";
+import Shareicon from "../assets/Svg/share.svg?react";
 
 const header = () => {
   const [sharebtn, setsharebtn] = useState(false);
