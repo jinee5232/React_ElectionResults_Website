551672a20f4ee61df520cce98aa29545de5a5b0d
jinee5232@gmail.com <jingyee0827@gmail.com>
2024-03-03 00:53:01 +0800

    total data

--- src/components/votelist.jsx
+++ src/components/votelist.jsx
@@ -17,18 +17,17 @@ const votelist = () => {
   const handlecountry = (e) => {
     const getcountryId = e.target.value;
     setTownname(null);
-    if (getcountryId !== "請選擇") {
-      const getStatedata = CITY_URL.find(
-        (country) => country.CityName === getcountryId
-      ).AreaList;
-      const filterData = getStatedata.filter((item) => {
-        if (item.area !== undefined) {
-          return item;
-        }
-      });
-      setTownname(filterData);
-      setShowdata(filterData);
-    }
+
+    const getStatedata = CITY_URL.find(
+      (country) => country.CityName === getcountryId
+    ).AreaList;
+    filterData();
+    const filterData = () => {
+      if (getStatedata !== undefined) {
+        setTownname(getStatedata);
+        setShowdata(getStatedata);
+      }
+    };
 
     document.getElementById("town").value = "請選擇"; // 或者您可以將 '' 替換為您想要的預設值
   };
