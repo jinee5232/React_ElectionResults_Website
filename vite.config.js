32cb18ff3bff9aed06a444ddfdf43a225ff988f0
jinee5232@gmail.com <jingyee0827@gmail.com>
2024-03-02 21:22:29 +0800

    install svgr

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import react from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
